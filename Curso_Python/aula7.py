70fab25c4d0694148b8a7ec0fcba1e226325b098
Kauã Lima <kaualimacampaner@gmail.com>
2025-02-24 10:32:27 -0300

    Third commit

---
+++ Curso_Python/aula7.py
@@ -0,0 +1,18 @@
+# Variáveis são usadas para salvar algo na memória do computador.
+# PEP8: inicie variáveis com letras minúsculas, pode usar
+# números e underline _.
+# O sinal de = é o operador de atribuição. Ele é usado para
+# atribuir um valor a um nome (variável).
+# Uso: nome_variavel = expressão
+
+# cidade = 'São Paulo'
+# total_itens = 5 + 3
+# booleano_exemplo = bool(0)
+# print(booleano_exemplo, type(booleano_exemplo))
+# print(cidade, total_itens)
+
+nome = 'Fulano'
+idade = 17
+maior_de_idade = idade >= 18
+print('Nome:', nome, 'Idade:', idade)
+print('É maior?', maior_de_idade)
---
+++ Curso_Python/aula9.py
@@ -0,0 +1,26 @@
+adicao = 20 + 15
+print('Adição:', adicao)
+
+subtracao = 50 - 18
+print('Subtração:', subtracao)
+
+multiplicacao = 7 * 6
+print('Multiplicação:', multiplicacao)
+
+divisao = 25 / 4  # float
+print('Divisão:', divisao)
+
+divisao_inteira = 25 // 4
+print('Divisão inteira:', divisao_inteira)
+
+exponenciacao = 3 ** 5
+print('Exponenciação:', exponenciacao)
+
+modulo = 77 % 5  # resto da divisão
+print('Módulo:', modulo)
+
+print(20 % 5 == 0)
+print(21 % 7 == 0)
+print(14 % 2 == 0)
+print(19 % 2 == 0)
+print(22 % 2 == 0)
