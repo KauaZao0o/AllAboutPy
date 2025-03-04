236d0e58ea15285b39a49e96f305bf9cb8649c63
Kauã Lima <kaualimacampaner@gmail.com>
2025-03-04 20:21:31 -0300

    Twelfth commit

---
+++ Curso_Python/aula35.py
@@ -0,0 +1,13 @@
+"""
+Repetições
+while (enquanto)
+Executa uma ação enquanto uma condição for verdadeira
+Loop infinito -> Quando um código não tem fim
+"""
+contador = 0
+
+while contador <= 10:
+    print(contador)  # Exibe o número antes de incrementar
+    contador += 1  # Incrementa de forma mais concisa
+
+print('Acabou')
